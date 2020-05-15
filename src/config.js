fc675e5dcfb538ff75ca853d5aea5818c0fe0f8f
William Schrader <wfschrad@gmail.com>
2020-05-15 10:03:20 -0700

    herokuTake2.

--- src/config.js
+++ src/config.js
@@ -7,10 +7,10 @@ export const newsUrl = `http://newsapi.org/v2/everything?q=bitcoin&from=2020-04-
 //     export const API = `http://localhost:8080/`
 // }
 export const API = `http://localhost:8080/`
-export const newsUrlBase = `http://newsapi.org/v2/`
-export const newsUrlSourceList = `http://newsapi.org/v2/sources`
+export const newsUrlBase = `https://newsapi.org/v2/`
+export const newsUrlSourceList = `https://newsapi.org/v2/sources`
 export const apiKEY = `78bf6a2d1a574d3995099f73fb8f6cd3`
-export const newsUrlTopCountry = `http://newsapi.org/v2/top-headlines?country=`
+export const newsUrlTopCountry = `https://newsapi.org/v2/top-headlines?country=`
 export const newsUrlTopCountryV1 = `https://newsapi.org/v1/articles?country=`
 
 
