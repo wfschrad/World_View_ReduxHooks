8ca20b22cc61f9be79dcba0864126fb7f323417f
William Schrader <wfschrad@gmail.com>
2020-05-15 10:52:27 -0700

    heroku4.

--- src/config.js
+++ src/config.js
@@ -1,11 +1,7 @@
 const production = process.env.NODE_ENV === "production";
 
 export const newsUrl = `http://newsapi.org/v2/everything?q=bitcoin&from=2020-04-30&sortBy=publishedAt&apiKey=78bf6a2d1a574d3995099f73fb8f6cd3`
-if (production) {
-    export const API = `https://rocky-inlet-52998.herokuapp.com/`
-} else {
-    export const API = `http://localhost:8080/`
-}
+export const API = production ? `https://rocky-inlet-52998.herokuapp.com/` : `http://localhost:8080/`
 export const newsUrlBase = `https://newsapi.org/v2/`
 export const newsUrlSourceList = `https://newsapi.org/v2/sources`
 export const apiKEY = `78bf6a2d1a574d3995099f73fb8f6cd3`
