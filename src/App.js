add0a01f939477c52721fdd39080e010b89ca0b1
William Schrader <wfschrad@gmail.com>
2020-05-18 02:12:21 -0700

    heroku4.

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import Results from './components/Results';
 import Swipable from './components/SwipableDrawer_M';
 import { AuthRoute, ProtectedRoute } from './components/utilRoutes';
 import { setUser } from './store/state';
-import Logout from './components/Logout';
+// import Logout from './components/Logout';
 import SavedStories from './components/SavedStories';
 
 
@@ -33,7 +33,7 @@ const App = () => {
         <AuthRoute path='/login' component={LoginForm} currentUserId={currentUserId} />
         <AuthRoute path='/signup' component={SignupForm} currentUserId={currentUserId} />
         <Route path='/showAll' component={Results} />
-        <Route path='/logout' component={Logout} />
+        {/* <Route path='/logout' component={Logout} /> */}
         <Route exact path='/' component={Home} />
         <Route path='/savedStories' component={SavedStories} />
       </Switch>
